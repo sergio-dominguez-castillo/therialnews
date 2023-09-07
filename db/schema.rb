6a45a2f399c90c02237dc3b3bd418164e1e16eef
Sergio Dominguez <sergio.castle@gmail.com>
2023-09-07 17:40:44 -0300

    feat: se genera migracion para tabla publication

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_040259) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_203814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "publications", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_publications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_040259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "publications", "users"
 end
